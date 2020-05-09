3b7c0b083bf22132a2425442d7092e086e3c62fc
Emerson Claudelino <tc.claudelino@kantaribopemedia.com>
2020-05-09 18:13:21 -0300

    elementos dom

--- functions.js
+++ functions.js
@@ -3,3 +3,25 @@ function soma(n1, n2){
 }
 
 console.log(soma(5,10));
+
+function botao(){
+    alert("você clicou");
+    document.getElementById("h3Clique").innerHTML = "<i>você cliclou<\i>";
+}
+
+function redirecionar(){
+    //window.open("https://github.com/ebilieri"); // abrir em nova janela
+    window.location.href = "https://github.com/ebilieri"; // abrir na mesma pagina
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Você passou o mouse";
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse";
+}
+
+function load(){
+    alert("página carregada");
+}
